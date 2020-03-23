a74a6b9d70dfc4f42830e2a697bb4b590b9cbfc6
DKT <b.apanasewicz@gmail.com>
2020-03-23 17:10:02 +0100

    diif

--- c64gfxconv-hs.go
+++ c64gfxconv-hs.go
@@ -9,8 +9,6 @@ import (
 	_ "image/png"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 )
 
 // ErrCheck - obsługa błedów
@@ -23,12 +21,6 @@ func ErrCheck(errNr error) bool {
 	return true
 }
 
-// // colorDistance - calculate color distance
-// // ================================================================================================
-// func colorDistance(c color.Color, pal []int) int {
-// 	return c.RGBA()
-// }
-
 // main - program entry point
 // ================================================================================================
 func main() {
@@ -37,72 +29,75 @@ func main() {
 	fmt.Println("= c64gfxconv-hs - C64 PNG -> Hires+Sprites gfx converter / Samar Productions 2020 =")
 	fmt.Println("===================================================================================")
 
-	inputFilename := flag.String("i", "", "input filename")
+	inputFilename1 := flag.String("u", "", "Unrestricted hires PNG filename")
+	inputFilename2 := flag.String("h", "", "Hires PNG filename")
+	outFilename := flag.String("o", "", "Difference PNG filename")
 
 	flag.Parse()
 
-	if *inputFilename != "" {
+	if *inputFilename1 != "" && *inputFilename2 != "" && *outFilename != "" {
 
-		fmt.Println("Loading file: ", *inputFilename)
+		fmt.Println("Loading files:  ", *inputFilename1, *inputFilename2)
 
-		// Read image from file that already exists
-		inputImageFile, err := os.Open(*inputFilename)
+		inputImageFile1, err := os.Open(*inputFilename1)
 		ErrCheck(err)
-		defer inputImageFile.Close()
+		defer inputImageFile1.Close()
+		inputImageFile2, err := os.Open(*inputFilename2)
+		ErrCheck(err)
+		defer inputImageFile2.Close()
 
-		// Calling the generic image.Decode() will tell give us the data
-		// and type of image it is as a string. We expect "png"
-		inputImage, inputFormat, err := image.Decode(inputImageFile)
+		imgUnres, inputFormat1, err := image.Decode(inputImageFile1)
+		ErrCheck(err)
+		imgHires, inputFormat2, err := image.Decode(inputImageFile2)
 		ErrCheck(err)
 
-		if inputFormat != "png" {
+		if inputFormat1 != "png" || inputFormat2 != "png" {
 			log.Fatalln("Only png images are supported")
 		}
 
-		size := inputImage.Bounds().Size()
+		size1 := imgUnres.Bounds().Size()
+		size2 := imgHires.Bounds().Size()
 
-		fmt.Println("Image size :  ", size.X, "x", size.Y)
+		fmt.Println("Image 1 size :  ", size1.X, "x", size1.Y)
+		fmt.Println("Image 2 size :  ", size2.X, "x", size2.Y)
 
-		if size.X != 320 && size.Y != 200 {
+		if size1.X != 320 && size1.Y != 200 || size2.X != 320 && size2.Y != 200 {
 			log.Fatalln("Only 320 x 200 pixel image size is supported")
 		}
 
 		// paletteC64 := []int{0x000000, 0xffffff, 0x68372B, 0x70A4B2, 0x6F3D86, 0x588D43, 0x352879, 0xB8C76F, 0x6F4F25, 0x433900, 0x9A6759, 0x444444, 0x6C6C6C, 0x9AD284, 0x6C5EB5, 0x959595}
 
-		rect := image.Rect(0, 0, size.X, size.Y)
+		rect := image.Rect(0, 0, 320, 200)
 		wImg := image.NewRGBA(rect)
 
-		// loop though all the x
-		for x := 0; x < size.X; x++ {
-			// and now loop thorough all of this x's y
-			for y := 0; y < size.Y; y++ {
-				pixel := inputImage.At(x, y)
-				originalColor := color.RGBAModel.Convert(pixel).(color.RGBA)
+		// Kolor nie występujacy w palecie dla pokazania miejsc gdzie musi być uzupełnione tło
+		//
+		indepColor := color.RGBA{0x20, 0x20, 0, 0xff}
 
-				// Offset colors a little, adjust it to your taste
-				r := float64(originalColor.R) * 0.92126
-				g := float64(originalColor.G) * 0.97152
-				b := float64(originalColor.B) * 0.90722
-				// average
-				grey := uint8((r + g + b) / 3)
-				c := color.RGBA{
-					R: grey, G: grey, B: grey, A: originalColor.A,
+		// loop though all the x
+		for x := 0; x < 320; x++ {
+			// and now loop thorough all of this x's y
+			for y := 0; y < 200; y++ {
+				pixelUnres := imgUnres.At(x, y)
+				pixelUnresColor := color.RGBAModel.Convert(pixelUnres).(color.RGBA)
+				pixelHires := imgHires.At(x, y)
+				pixelHiresColor := color.RGBAModel.Convert(pixelHires).(color.RGBA)
+
+				if pixelUnresColor != pixelHiresColor {
+					wImg.Set(x, y, pixelUnresColor)
+				} else {
+					wImg.Set(x, y, indepColor)
 				}
-				wImg.Set(x, y, c)
 			}
 		}
 
-		ext := filepath.Ext(*inputFilename)
-		name := strings.TrimSuffix(filepath.Base(*inputFilename), ext)
-		newImagePath := fmt.Sprintf("%s_gray%s", name, ext)
-		fg, err := os.Create(newImagePath)
+		fg, err := os.Create(*outFilename)
 		defer fg.Close()
 		ErrCheck(err)
 		err = png.Encode(fg, wImg)
 		ErrCheck(err)
 
-		// colorDistance(inputImage.At(0, 0), paletteC64)
-		// fmt.Println()
+		// Budowa sprajtów
 
 	} else {
 		fmt.Println("Use -h flag to read usage")
